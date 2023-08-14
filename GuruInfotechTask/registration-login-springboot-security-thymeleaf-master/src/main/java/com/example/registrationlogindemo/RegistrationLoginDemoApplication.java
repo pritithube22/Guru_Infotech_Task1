05e75524f24426b3f14a6a17b7942eb2a737dd6f
Priti Thube <137392329+pritithube22@users.noreply.github.com>
2023-08-14 21:30:34 +0530

    Add files via upload

---
+++ GuruInfotechTask/registration-login-springboot-security-thymeleaf-master/src/main/java/com/example/registrationlogindemo/RegistrationLoginDemoApplication.java
@@ -0,0 +1,41 @@
+package com.example.registrationlogindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationLoginDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationLoginDemoApplication.class, args);
+		System.out.println("Application Running Successfully.......");
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
